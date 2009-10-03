ea4cc7b7578b885e1dee5168e71b60daad6c3280
Masayuki Igawa <masayuki.igawa@gmail.com>
2009-10-04 07:25:31 +0900

    Fix the warnings about the DataMapper types.

--- lib/user.rb
+++ lib/user.rb
@@ -5,20 +5,20 @@ require 'dm-timestamps'
 class User
     include DataMapper::Resource
 
-    property :user_id, Integer, :serial => true
-    property :twitter_token, String, :size => 500
-    property :twitter_secret, String, :size => 500
-    property :mixi_email, String, :size => 500
-    property :mixi_password, String, :size => 500
-    property :hatena_id, String, :size => 500
-    property :hatena_haiku_password, String, :size => 500
-    property :wasser_id, String, :size => 500
-    property :wasser_password, String, :size => 500
-    property :gcal_feed_url, String, :size => 500
-    property :gcal_mail, String, :size => 500
-    property :gcal_password, String, :size => 500
-    property :echo_twitter_url, String, :size => 1
-    property :twitter_url, String, :size => 30
+    property :user_id, Serial
+    property :twitter_token, String, :length => 500
+    property :twitter_secret, String, :length => 500
+    property :mixi_email, String, :length => 500
+    property :mixi_password, String, :length => 500
+    property :hatena_id, String, :length => 500
+    property :hatena_haiku_password, String, :length => 500
+    property :wasser_id, String, :length => 500
+    property :wasser_password, String, :length => 500
+    property :gcal_feed_url, String, :length => 500
+    property :gcal_mail, String, :length => 500
+    property :gcal_password, String, :length => 500
+    property :echo_twitter_url, String, :length => 1
+    property :twitter_url, String, :length => 30
     property :last_tweeted_at, DateTime
     property :created_at, DateTime
     property :updated_at, DateTime
